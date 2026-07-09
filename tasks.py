81d4840449e9e4a2b1fb491673557859a9ce7879
Dennis Noll <dennis.noll@cern.ch>
2026-07-08 17:55:45 -0700

    [dev] XHH: 280->300

--- tasks.py
+++ tasks.py
@@ -931,7 +931,7 @@ class PlotEventsWrapper(ProcessorMixin, BaseTask):
                     "WN_HyyN_600",
                     "XSH_500_100",
                     "XSH_750_100_ll",
-                    "XHH_280",
+                    "XHH_300",
                     "XHH_500",
                     "XHH_1000",
                     "ZpHyyA_200",
